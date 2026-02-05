d2703638c27446e0567febe891cf0c0dfee6673e
Game-one <abupu9@gmail.com>
2026-02-06 01:25:39 +0700

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,19 +1,15 @@
 const fs = require("fs");
-
-const DB_FILE = "./db.json";
+const path = "./db.json";
 
 function loadDB() {
-  if (!fs.existsSync(DB_FILE)) {
-    fs.writeFileSync(
-      DB_FILE,
-      JSON.stringify({ users: {} }, null, 2)
-    );
+  if (!fs.existsSync(path)) {
+    return { users: {} };
   }
-  return JSON.parse(fs.readFileSync(DB_FILE));
+  return JSON.parse(fs.readFileSync(path));
 }
 
 function saveDB(db) {
-  fs.writeFileSync(DB_FILE, JSON.stringify(db, null, 2));
+  fs.writeFileSync(path, JSON.stringify(db, null, 2));
 }
 
 module.exports = { loadDB, saveDB };
